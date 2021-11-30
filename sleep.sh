b9c2b8260e37108d27780377810843203776f00c
Raz Ben Simon <razbensimon2@gmail.com>
2021-11-30 12:13:32 +0200

    check for file existance

--- sleep.sh
+++ sleep.sh
@@ -6,4 +6,14 @@ echo "hello from root script.sh"
 sleep ${SLEEP_IN_SECONDS:-3}
 
 ls $ENV0_TEMPLATE_DIR
-cat $ENV0_TEMPLATE_DIR/env0.yml
+test -f /etc/resolv.conf && echo "$FILE exists."
+
+FILE=$ENV0_TEMPLATE_DIR/env0.yml
+if [[ -f "$FILE" ]]; then
+    cat $FILE
+fi
+
+FILE=$ENV0_TEMPLATE_DIR/env0.yaml
+if [[ -f "$FILE" ]]; then
+    cat $FILE
+fi
