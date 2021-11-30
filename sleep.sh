920771256c195b3b1bf3cd0815db63fbb85f689e
Raz Ben Simon <razbensimon2@gmail.com>
2021-11-30 12:15:32 +0200

    ...

--- sleep.sh
+++ sleep.sh
@@ -6,7 +6,6 @@ echo "hello from root script.sh"
 sleep ${SLEEP_IN_SECONDS:-3}
 
 ls $ENV0_TEMPLATE_DIR
-test -f /etc/resolv.conf && echo "$FILE exists."
 
 FILE=$ENV0_TEMPLATE_DIR/env0.yml
 if [[ -f "$FILE" ]]; then
