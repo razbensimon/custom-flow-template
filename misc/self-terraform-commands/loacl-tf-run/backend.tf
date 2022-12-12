f6facf3ff9b4ffcccd9de79898451c5462773b7f
Raz Ben Simon <razbensimon2@gmail.com>
2022-12-12 21:32:05 +0200

    no strict version

--- misc/self-terraform-commands/loacl-tf-run/backend.tf
+++ misc/self-terraform-commands/loacl-tf-run/backend.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version  = ">= 1.3.4"
-
   backend "remote" {
     hostname = "backend.api.env0.com"
     organization = "${data.env_variable.env0_proj_id.value}.${data.env_variable.env0_proj_id.value}"
