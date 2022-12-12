60ab0bcf1446bb7496726fc0901cb2782c1a5d8e
Raz Ben Simon <razbensimon2@gmail.com>
2022-12-12 21:55:56 +0200

    controlled

--- misc/self-terraform-commands/loacl-tf-run/backend.tf
+++ misc/self-terraform-commands/loacl-tf-run/backend.tf
@@ -4,7 +4,7 @@ terraform {
     organization = "1ffc3f01-9501-4577-87e7-6ea8e5b4d74b.bac0dd9a-23e7-4d17-a726-4931ef301eac"
 
     workspaces {
-      name = "testing-self-tf-commands-1"
+      name = "testing-self-tf-commands-1-controlled"
     }
   }
 }
