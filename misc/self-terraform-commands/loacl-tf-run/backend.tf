91ca0ebbad03b3544ddf77e02b6479f47c074cd3
Raz Ben Simon <razbensimon2@gmail.com>
2022-12-12 21:46:26 +0200

    no variables :(

--- misc/self-terraform-commands/loacl-tf-run/backend.tf
+++ misc/self-terraform-commands/loacl-tf-run/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "backend.api.env0.com"
-    organization = "${data.env_variable.env0_proj_id.value}.${data.env_variable.env0_proj_id.value}"
+    organization = "1ffc3f01-9501-4577-87e7-6ea8e5b4d74b.bac0dd9a-23e7-4d17-a726-4931ef301eac"
 
     workspaces {
       name = "testing-self-tf-commands-1"
