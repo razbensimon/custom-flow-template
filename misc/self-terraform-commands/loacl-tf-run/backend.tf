ef9115219952a957bd78774f6e9877f7675e76a1
Raz Ben Simon <razbensimon2@gmail.com>
2022-12-12 21:22:39 +0200

    add self-terraform-commands scenario

---
+++ misc/self-terraform-commands/loacl-tf-run/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version  = ">= 1.3.4"
+
+  backend "remote" {
+    hostname = "backend.api.env0.com"
+    organization = "${data.env_variable.env0_proj_id.value}.${data.env_variable.env0_proj_id.value}"
+
+    workspaces {
+      name = "my-test-env-1"
+    }
+  }
+}
---
+++ misc/self-terraform-commands/loacl-tf-run/variables.tf
@@ -0,0 +1,7 @@
+data "env_variable" "env0_org_id" {
+  name = "ENV0_ORGANIZATION_ID"
+}
+
+data "env_variable" "env0_proj_id" {
+  name = "ENV0_PROJECT_ID"
+}
