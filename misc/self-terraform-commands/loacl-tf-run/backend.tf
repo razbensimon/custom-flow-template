5d4783b7e57eb0288df5da61d398d96c971b4b31
razbensimon <raz.bensimon@env0.com>
2022-12-21 12:16:39 +0200

    run fmt

--- misc/self-terraform-commands/loacl-tf-run/backend.tf
+++ misc/self-terraform-commands/loacl-tf-run/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend "remote" {    
+  backend "remote" {
     # hostname = "backend${xxx ? "" : "-STAGE"}.api${ENV0_STAGE !== 'prod' ? '.dev' : ''}.env0.com"
     hostname = "backend-pr10424.api.dev.env0.com"
     #organization = "1ffc3f01-9501-4577-87e7-6ea8e5b4d74b.bac0dd9a-23e7-4d17-a726-4931ef301eac"
@@ -12,7 +12,7 @@ terraform {
 
   required_providers {
     env = {
-      source = "tchupp/env"
+      source  = "tchupp/env"
       version = "0.0.2"
     }
   }
--- misc/self-terraform-commands/loacl-tf-run/main.tf
+++ misc/self-terraform-commands/loacl-tf-run/main.tf
@@ -1,3 +1,3 @@
 resource "null_resource" "null" {
-    count = tonumber(data.env_variable.count_nulls.value) > 1 ? tonumber(data.env_variable.count_nulls.value) : 1
+  count = tonumber(data.env_variable.count_nulls.value) > 1 ? tonumber(data.env_variable.count_nulls.value) : 1
 }
